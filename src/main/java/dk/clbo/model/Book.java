25759823e239fcff249a1c984b0d428298c36aff
clbokea <clbo@kea.dk>
2020-10-08 09:46:18 +0200

    init

--- src/main/java/dk/clbo/model/Book.java
+++ src/main/java/dk/clbo/model/Book.java
@@ -1,14 +1,20 @@
 package dk.clbo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Book {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    @ManyToOne
+    Author author;
+
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO author ( FIRST_NAME, LAST_NAME) VALUES ('Claus', 'Bove');
+INSERT INTO book (title, isbn, publisher, AUTHOR_ID) VALUES ('Cook Book', 12345, 'Kea', 1);
+INSERT INTO book (title, isbn, publisher, AUTHOR_ID) VALUES ('MC Book', 12345, 'MV', 1);
+INSERT INTO book (title, isbn, publisher, AUTHOR_ID) VALUES ('JAva Spring', 543534, 'Springer', 1);
