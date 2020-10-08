46e96f1f02139464a9996af2083efe99823207eb
clbokea <clbo@kea.dk>
2020-10-08 09:17:52 +0200

    init

---
+++ src/main/java/dk/clbo/model/Author.java
@@ -0,0 +1,18 @@
+package dk.clbo.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Author {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
+
---
+++ src/main/java/dk/clbo/model/Book.java
@@ -0,0 +1,14 @@
+package dk.clbo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    private Long id;
+
+}
